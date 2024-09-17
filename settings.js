8b8cc48a7d9744689d8084fbf67dbd7b84fc9a1d
Hubdarkweb <klenin980@gmail.com>
2024-09-17 16:26:01 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "254112386921"
+global.ownername = "🌊TOpPLUG"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '254112386921'  //creator number
+global.ownername = '🌊TOpPLUG' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
@@ -29,7 +29,7 @@ global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/09VagJIAr3bbVBCpEkAM07'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
